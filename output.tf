91b9ac9d5582ad946a9a18be426153c081ce668f
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-06-10 19:04:53 -0300

    change outputs

--- output.tf
+++ output.tf
@@ -1,17 +1,37 @@
-output "root_ca_key" {
-  value     = tls_private_key.linkerd_root_ca.private_key_pem
+output "ca_key" {
   sensitive = true
+  value     = tls_private_key.linkerd_root_ca.private_key_pem
 }
 
-output "root_ca_crt" {
+output "ca_crt" {
   value = tls_self_signed_cert.linkerd_root_ca.cert_pem
 }
 
+output "ca_crt_validity" {
+  value = tls_self_signed_cert.linkerd_root_ca.validity_end_time
+}
+
 output "issuer_key" {
-  value     = tls_private_key.linkerd_issuer.private_key_pem
   sensitive = true
+  value     = tls_private_key.linkerd_issuer.private_key_pem
 }
 
 output "issuer_crt" {
   value = tls_locally_signed_cert.linkerd_issuer.cert_pem
+}
+
+output "issuer_crt_validity" {
+  value = tls_locally_signed_cert.linkerd_issuer.validity_end_time
+}
+
+output "linkerd_config" {
+  sensitive = true
+  value = {
+    ca_key            = tls_self_signed_cert.linkerd_root_ca.cert_pem
+    ca_crt            = tls_self_signed_cert.linkerd_root_ca.cert_pem
+    ca_crt_validity   = tls_self_signed_cert.linkerd_root_ca.validity_end_time
+    issuer_key        = tls_private_key.linkerd_issuer.private_key_pem
+    issuer_crt        = tls_locally_signed_cert.linkerd_issuer.cert_pem
+    issuer_crt_expiry = tls_locally_signed_cert.linkerd_issuer.validity_end_time
+  }
 }
