3833c32175e0e7d55c7ca4fcfc91b3b0c30f072e
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2022-11-21 23:05:12 -0300

    validate

--- variables.tf
+++ variables.tf
@@ -9,4 +9,13 @@ variable "linkerd-viz" {
     username = ""
     password = ""
   }
+
+  validation {
+    condition = (
+      var.linkerd-viz.username == "" && var.linkerd-viz.password == ""
+      ) || (
+      var.linkerd-viz.username != "" && var.linkerd-viz.password != ""
+    )
+    error_message = "linkerd-viz:e either none or both vars username and password must be defined."
+  }
 }
